e26536ff5afa78d1406c2aa6e3882122aaf552f6
Kacper Stroisz <kacperstroisz@wp.pl>
2018-01-27 14:37:16 +0100

    Add /js

---
+++ js/javascript.js
@@ -0,0 +1,18 @@
+<script type="text/javascript"> 
+  // To jest podobno najprostszy skrypt w javaScript 
+  alert("Hello!"); 
+</script>
+
+
+<script>
+/*
+Tutaj można wpisać sporo
+informacji, które nie będą miały
+wpływu na działanie programu.
+*/
+</script>
+
+<script type="text/javascript">
+// Komunikat pojawi się sam w przeglądarce
+alert("Ale jazda");
+</script>
